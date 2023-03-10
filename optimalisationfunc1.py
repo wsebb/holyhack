e888ce94e7e3ce27ae773cd44fe12a81acfa2ecf
sennestans <82170840+sennestans@users.noreply.github.com>
2023-03-11 00:51:51 +0100

    implented functions

--- optimalisationfunc1.py
+++ optimalisationfunc1.py
@@ -16,7 +16,7 @@ def optimisationfunc(review):
     aantal_scheldwoorden = get_aantal_scheldwoorden(review_text)
 
     #weights
-    value = 1 * likes + 1* aantal_woorden - 1 * aantal_scheldwoorden
+    value = 1 * likes + 1 * aantal_woorden - 1 * aantal_scheldwoorden
     if premium or trial or subscription:
         value += 1
     return value
@@ -26,7 +26,41 @@ def get_review_text(dict_reviews):
 
 def get_premium(review_text):
     woorden = review_text.split()
-    if 'premium' in woorden:
+    list_of_premium = []
+    for i in range(len(woorden)):
+        for j in range(len(list_of_premium)):
+            if list_of_premium[j] == woorden[i]:
+                return True
+    return False
 
+def get_trial(review_text):
+    woorden = review_text.split()
+    list_of_trial = []
+    for i in range(len(woorden)):
+        for j in range(len(list_of_trial)):
+            if list_of_trial[j] == woorden[i]:
+                return True
+    return False
 
+def get_subscription(review_text):
+    woorden = review_text.split()
+    list_of_subscription = []
+    for i in range(len(woorden)):
+        for j in range(len(list_of_subscription)):
+            if list_of_subscription[j] == woorden[i]:
+                return True
+    return False
 
+def get_aantal_woorden(review_text):
+    woorden = review_text.split()
+    return len(woorden)
+
+def get_aantal_scheldwoorden(review_text):
+    woorden = review_text.split()
+    list_of_scheldwoorden = []
+    aantal = 0
+    for i in range(len(woorden)):
+        for j in range(len(list_of_scheldwoorden)):
+            if list_of_scheldwoorden[j] == woorden[i]:
+                aantal += 1
+    return aantal
